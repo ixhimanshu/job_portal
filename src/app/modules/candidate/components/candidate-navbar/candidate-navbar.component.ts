5ac5d4d5576d5e77e2545fb0d1a86c781e66b696
Himanshu <himanshu.thakur@mobicules.com>
2021-02-08 16:54:25 +0530

    --modules setup

---
+++ src/app/modules/candidate/components/candidate-navbar/candidate-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-navbar',
+  templateUrl: './candidate-navbar.component.html',
+  styleUrls: ['./candidate-navbar.component.scss']
+})
+export class CandidateNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
